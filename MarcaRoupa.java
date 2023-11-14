a09df5c5bbbf0275fd0210f07393ffdbfb7507d3
juju <132360412+jumduarte@users.noreply.github.com>
2023-11-14 11:28:08 -0300

    Add files via upload

---
+++ MarcaRoupa.java
@@ -0,0 +1,12 @@
+package Roupa;
+
+public enum MarcaRoupa {
+	CHANNEL,
+	YVES_SAINT_LAURENT,
+	VIVIENNE_WESTWOOD,
+	VERSACE,
+	PRADA,
+	DIOR,
+	MIU_MIU
+
+}
---
+++ PrincipalRoupa.java
@@ -0,0 +1,29 @@
+package Roupa;
+
+public class PrincipalRoupa {
+
+	public static void main(String[] args) {
+		MarcaRoupa marca = MarcaRoupa.YVES_SAINT_LAURENT;
+		System.out.println(marca); //YVES_SAINT_LAURENT
+		
+		MarcaRoupa channel = MarcaRoupa.CHANNEL;
+		System.out.println(channel);
+		
+		MarcaRoupa dior = MarcaRoupa.DIOR;
+		System.out.println(dior);
+		
+		MarcaRoupa miumiu = MarcaRoupa.MIU_MIU;
+		System.out.println(miumiu);
+		
+		MarcaRoupa prada = MarcaRoupa.PRADA;
+		System.out.println(prada);
+		
+		MarcaRoupa versace = MarcaRoupa.VERSACE;
+		System.out.println(versace);
+		
+		MarcaRoupa vw = MarcaRoupa.VIVIENNE_WESTWOOD;
+		System.out.println(vw);
+
+	}
+
+}
