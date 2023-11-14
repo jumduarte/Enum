0ff9ece67d41f01aa3d3763b2fcdcbde551717c9
juju <132360412+jumduarte@users.noreply.github.com>
2023-11-14 11:27:48 -0300

    Add files via upload

---
+++ MarcaCarro.java
@@ -0,0 +1,17 @@
+package Carro;
+
+public enum MarcaCarro {
+	
+	PORSCHE,
+	FERRARI,
+	PAGANI,
+	BUGATTI,
+	LAMBORGHINI,
+	NISSAN,
+	TOYOTA,
+	MERCEDES,
+	BMW,
+	MASERATTI
+	
+
+}
---
+++ PrincipalCarro.java
@@ -0,0 +1,36 @@
+package Carro;
+
+public class PrincipalCarro {
+	public static void main(String[] args) {
+		MarcaCarro marca = MarcaCarro.PORSCHE;
+		System.out.println(marca); // PORSCHE
+		
+		MarcaCarro mc = MarcaCarro.FERRARI;
+		System.out.println(mc); // FERRARI
+		
+		MarcaCarro modelo = MarcaCarro.BMW;
+		System.out.println(modelo); // BMW
+		
+		MarcaCarro carro = MarcaCarro.BUGATTI;
+		System.out.println(carro); // BUGATTI
+		
+		MarcaCarro car = MarcaCarro.LAMBORGHINI;
+		System.out.println(car); // LAMBORGHINI
+		
+		MarcaCarro md = MarcaCarro.MASERATTI;
+		System.out.println(md); // MESERATTI
+		
+		MarcaCarro carroo = MarcaCarro.MERCEDES;
+		System.out.println(carroo); // MERCEDES;
+		
+		MarcaCarro nissan = MarcaCarro.NISSAN;
+		System.out.println(nissan);
+		
+		MarcaCarro pagani = MarcaCarro.PAGANI;
+		System.out.println(pagani);
+		
+		MarcaCarro toyota = MarcaCarro.TOYOTA;
+		System.out.println(toyota);
+	}
+
+}
